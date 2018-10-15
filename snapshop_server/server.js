f02f67b988c0370f24dd97f40ae528f0037b5711
sarathyt <sarathy.t@husky.neu.edu>
2018-10-14 20:56:07 -0400

    2nd Commit

---
+++ snapshop_server/server.js
@@ -0,0 +1,43 @@
+let express = require('express'),
+    app = express(),
+    port = process.env.PORT || 3000,
+    mongoose = require('mongoose'), //created model loading here
+    bodyParser = require('body-parser');
+    path=require('path');
+
+// mongoose instance connection url connection
+mongoose.connect('mongodb://localhost:27017/snapshopDB', {
+    useMongoClient: true
+});
+mongoose.Promise = global.Promise;
+console.log(mongoose.Promise);
+//Adding body parser for handling request and response objects.
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+// app.use(bodyParser.urlencoded({
+// extended: true
+// }));
+
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname,'public')));
+
+// app.get('/*',(req,res) =>{
+// res.sendFile(path.join(__dirname,'/public/index.html'))
+// })
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept,Accept-Language");
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    next();
+});
+
+//Initialize app
+let initApp = require('./api/app');
+initApp(app);
+
+app.listen(port);
+console.log('SnapShop RESTful API server started on: ' + port);
